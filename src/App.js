917922f1f233fa1e8db3b7bc8ff5fd26339c9ec8
AnilPandiya <anilpandiya05@gmail.com>
2022-04-27 17:17:18 +0530

    Anil | added routing part

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-import Dashboard from './Components/Dashboard/Dashboard';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Dashboard from "./Components/Dashboard/Dashboard";
+import Doctor from "./Components/Doctor/Doctor";
+import Patient from "./Components/Patient/Patient";
 
 function App() {
   return (
-    <Dashboard />
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Dashboard/>} />
+        <Route path="/doctor" element={<Doctor/>} />
+        <Route path="/patient" element={<Patient/>} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/Components/Dashboard/Dashboard.jsx
+++ src/Components/Dashboard/Dashboard.jsx
@@ -1,14 +1,16 @@
 import { Button } from '@mobiscroll/react';
+import { useNavigate  } from 'react-router-dom';
 import './Dashboard.scss';
 import '@mobiscroll/react/dist/css/mobiscroll.min.css';
 
 function Dashboard() {
+  const navigate = useNavigate();
   return (
     <div className="dashboard">
        My Medical HUB
        <div>
-        <Button color="primary" variant="outline">Doctor</Button>
-        <Button color="primary" variant="outline">Patient</Button>
+        <Button color="primary" variant="outline" onClick={() => navigate('/doctor')}>Doctor</Button>
+        <Button color="primary" variant="outline" onClick={() => navigate('/patient')}>Patient</Button>
        </div>
     </div>
   );
